72004ab631e4485dbcbe1a54cab53fef4d406658
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-03 04:24:45 -0400

    lisen articlus with about

---
+++ app.js
@@ -0,0 +1,7 @@
+// about
+// all my button
+// all my articles
+
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
