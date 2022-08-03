2945d98ea413acc25037b738bd332515ea3d3c3b
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-03 04:31:18 -0400

    geting target id

--- app.js
+++ app.js
@@ -5,3 +5,7 @@
 const btns = document.querySelectorAll('.tab-btn');
 const about = document.querySelector('.about');
 const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function(e){
+  const id = e.target.dataset.id;
+});
